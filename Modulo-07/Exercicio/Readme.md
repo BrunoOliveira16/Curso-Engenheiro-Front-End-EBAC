0a5d2ba4d98d327edb45947e5be555e8493bf1b5
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-05-28 10:48:11 -0300

    Feat: Criado formulario A/B exercicio modulo 07

---
+++ Modulo-07/Exercicio/Readme.md
@@ -0,0 +1,10 @@
+## A entrega desse exercício consiste em:
+- Crie um formulário HTML que possua dois campos numéricos, campo A e campo B e um botão para submeter o formulário.
+
+- Crie uma validação no JavaScript, onde o formulário será válido caso o número B seja maior que o número A.
+
+- Exiba uma mensagem positiva quando o formulário for válido e uma mensagem negativa quando for inválido.
+
+- Suba no Github no repositório do curso em uma branch chamada exercicio_html_js.
+
+- Copie o link do repositório na plataforma da EBAC.
---
+++ Modulo-07/Exercicio/assets/js/main.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+const inputNumberA = document.getElementById('number-a');
+const inputNumberB = document.getElementById('number-b');
+const message = document.getElementById('message');
+let formIsValid = false;
+
+// Formulario com menssagem de sucesso
+function successfulForm () {
+    message.classList.add('success');
+    message.classList.remove('error');
+    inputNumberB.classList.remove('input-error');
+    message.innerHTML = `Formulario enviado com sucesso!`;
+    inputNumberA.value = '';
+    inputNumberB.value = '';
+}
+
+// Formulario com mensagem de erro
+function formWithError () {
+    message.classList.add('error');
+    inputNumberB.classList.add('input-error');
+    message.innerHTML = `Número B menor que A, formulário não enviado!`;
+}
+
+// Validando o envio do formulário
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const valueA = inputNumberA.value;
+    const valueB = inputNumberB.value;
+
+    if(valueB > valueA) {
+        successfulForm();
+    } else {
+        formWithError();
+    }
+})
+
