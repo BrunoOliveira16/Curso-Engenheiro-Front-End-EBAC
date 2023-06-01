eebb9907e55e4e2649555a43008a494536cec06c
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-06-01 17:54:46 -0300

    Feat: Implementado mask e validate no form

--- Modulo-10/ebac-motors/assets/js/main.js
+++ Modulo-10/ebac-motors/assets/js/main.js
@@ -2,4 +2,45 @@ $(document).ready(function() {
     $('.menu-hamburguer').click(function(){
         $('nav').slideToggle();
     });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(xx) xxxxx-xxxx',
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoDeInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            telefone: 'Por favor, insira seu telefone',
+            email: 'Por favor, insira seu email'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            } else {
+
+            }
+        }
+    })
 });
