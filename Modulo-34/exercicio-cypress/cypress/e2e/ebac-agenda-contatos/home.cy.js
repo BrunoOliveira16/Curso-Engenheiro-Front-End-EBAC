0038443e6509ace2206d61433958d45c092c215e
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-14 20:38:26 -0300

    :sparkles: Feat: criado branch exercicio-cypress

---
+++ Modulo-34/exercicio-cypress/cypress/e2e/ebac-agenda-contatos/home.cy.js
@@ -0,0 +1,13 @@
+/// <reference types ="cypress" />
+
+describe('', () => {
+    beforeEach(() => {
+        cy.visit("https://agenda-contatos-react.vercel.app/")
+    })
+
+    it('Deve inserir um contato na lista', () => {})
+
+    it('Deve alterar um contato na lista', () => {})
+
+    it('Deve remover um contato na lista', () => {})
+})
