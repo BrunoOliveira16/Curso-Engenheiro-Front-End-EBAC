98b32d8b2eb9aec83397de8d67a726510c681bfd
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-14 23:59:14 -0300

    :sparkles: Feat: criado testes com cypress

--- Modulo-34/exercicio-cypress/cypress/e2e/ebac-agenda-contatos/home.cy.js
+++ Modulo-34/exercicio-cypress/cypress/e2e/ebac-agenda-contatos/home.cy.js
@@ -5,9 +5,24 @@ describe('', () => {
         cy.visit("https://agenda-contatos-react.vercel.app/")
     })
 
-    it('Deve inserir um contato na lista', () => {})
+    it('Deve inserir um contato na lista', () => {
+        cy.get('[type="text"]').type('Bruno Oliveira')
+        cy.get('[type="email"]').type('bruno@teste.com')
+        cy.get('[type="tel"]').type('21987654321')
+        cy.get('.adicionar').click()
+        cy.wait(2000)
+    })
 
-    it('Deve alterar um contato na lista', () => {})
+    it('Deve alterar um contato na lista', () => {
+        cy.get(':nth-child(3) > .sc-gueYoa > .edit').click()
+        cy.get('[type="text"]').clear().type('Usuario00')
+        cy.get('[type="email"]').clear().type('email@teste.com')
+        cy.get('[type="tel"]').clear().type('11234568888')
+        cy.get('.alterar').click()
+        cy.wait(2000)
+    })
 
-    it('Deve remover um contato na lista', () => {})
+    it('Deve remover um contato na lista', () => {
+        cy.get(':nth-child(2) > .sc-gueYoa > .delete').click()
+    })
 })
